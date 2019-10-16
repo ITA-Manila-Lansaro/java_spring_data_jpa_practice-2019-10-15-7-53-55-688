209da21e47807dfd678c43933b5e0b144ae28e0c
robertnajiblansangan <robert.najib.lansangan@oocl.com>
2019-10-16 17:00:00 +0800

    [Najib & Mike] Update and Delete

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -3,7 +3,10 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.naming.CompositeName;
 
 @RestController
 @RequestMapping("/companies")
@@ -14,14 +17,30 @@ public class CompanyController {
 
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-
         return companyRepository.findAll();
     }
 
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-
         return companyRepository.save(company);
-
     }
+
+    @GetMapping(value= "/{id}", produces = {"application/json"})
+    public Company getCompanyById (@PathVariable Long id){
+        return companyRepository.findCompanyById(id);
+    }
+
+    @PutMapping(produces = {"application/json"})
+    public Company updateCompany (@RequestBody Company company){
+        return companyRepository.save(company);
+    }
+
+    @ResponseStatus(HttpStatus.GONE)
+    @DeleteMapping(value = "/{id}" , produces = {"application/json"})
+    public void deleteCompany (@PathVariable Long id){
+        companyRepository.deleteById(id);
+    }
+
+
+
 }
--- src/main/java/com/tw/apistackbase/repository/CompanyProfileRepository.java
+++ src/main/java/com/tw/apistackbase/repository/CompanyProfileRepository.java
@@ -1,7 +1,9 @@
 package com.tw.apistackbase.repository;
 
+import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.core.CompanyProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyProfileRepository extends JpaRepository <CompanyProfile, Long> {
+    Company findNameById(Long id);
 }
